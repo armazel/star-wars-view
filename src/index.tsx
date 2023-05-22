12b8347ee0aeebd90d89f1b277cbde2513d0f714
Egor Dragun <egor.dragun@credentially.io>
2023-05-22 07:16:02 +0400

    remove style

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-// import "antd/dist/reset.scss";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";

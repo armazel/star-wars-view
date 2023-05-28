7b919dabe5b0c8fbf7174833cb96e0424f4812d3
armazel <dragun.egor@gmail.com>
2023-05-28 19:52:13 +0400

    exclude controll pannel update debounce

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -20,7 +20,7 @@ const Search: React.FC<SearchParams> = ({
     onLoad,
     searchData = "",
 }) => {
-    const debounceTimeout = 1500;
+    const debounceTimeout = 800;
 
     const loadData = (e: ChangeEvent<HTMLInputElement>) => {
         const inputValue = e.target.value;
